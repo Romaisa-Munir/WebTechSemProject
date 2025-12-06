91dd662670a7a47a49fd52fdba1610cf87dd156c
Romaisa Munir <romaisa.munir571@gmail.com>
2025-12-06 16:54:14 +0000

    Fix: Set correct fallback API URL

--- client/Final/src/components/Auth.jsx
+++ client/Final/src/components/Auth.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './auth.css';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://13.201.96.168:5001';
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true);
